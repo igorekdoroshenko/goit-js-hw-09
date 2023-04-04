33e7c3a713cf5a4808be21e9974330c89c978c5d
igorekdoroshenko <igorek.doroshenko@gmail.com>
2023-04-04 21:47:18 +0300

    01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
 
+let timerId = null;
+
+buttonStart.addEventListener('click', onStartClick);
+buttonStop.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  if (!timerId) {
+    buttonStart.disabled = true; // Вимикаємо кнопку "Start"
+    timerId = setInterval(changeBackgroundColor, 1000);
+  }
+}
+
+function onStopClick() {
+  if (timerId) {
+    clearInterval(timerId);
+    timerId = null;
+    buttonStart.disabled = false; // Вмикаємо кнопку "Start" знову
+  }
+}
+
+function changeBackgroundColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
